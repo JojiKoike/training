e80ce2c9f20f073cc79f02bad232d902a98c6fa2
JojiKoike <5671422+JojiKoike@users.noreply.github.com>
2024-07-27 17:55:03 +0900

    ✨ Exercise 2

--- shellgei160/qdata/2/img/image_generator.bash
+++ shellgei160/qdata/2/img/image_generator.bash
@@ -4,5 +4,5 @@ while read -r _color ; do
   i=$(( i + 1 ))
   fname="${i}_${_color}.png"
   echo "${fname}"
-  convert -size 1024x1024 xc:"${_color}" "${fname}"
+  magick -size 1024x1024 xc:"${_color}" "${fname}"
 done < <( gshuf -n60 -er black grey white green yellow red blue tomato steelblue green purple orange brown )
